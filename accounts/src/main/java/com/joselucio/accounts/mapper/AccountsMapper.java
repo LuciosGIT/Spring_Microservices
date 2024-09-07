c2c887dba17e01c926b5458fdd9eb85716a9e67f
LuciosGIT <jloliveirajunior1903@gmail.com>
2024-09-07 18:15:45 -0300

    Implementing AccountsMapper

---
+++ accounts/src/main/java/com/joselucio/accounts/mapper/AccountsMapper.java
@@ -0,0 +1,23 @@
+package com.joselucio.accounts.mapper;
+
+import com.joselucio.accounts.dto.AccountsDto;
+import com.joselucio.accounts.entity.Accounts;
+
+public class AccountsMapper {
+
+    public static AccountsDto mapToAccountsDto(Accounts accounts, AccountsDto accountsDto) {
+
+        accountsDto.setAccountNumber(accounts.getAccountNumber());
+        accountsDto.setAccountType(accounts.getAccountType());
+        accountsDto.setBranchAdress(accounts.getBranchAdress());
+        return accountsDto;
+    }
+
+    public static Accounts mapToAccounts(AccountsDto accountsDto, Accounts accounts) {
+        accounts.setAccountNumber(accountsDto.getAccountNumber());
+        accounts.setAccountType(accountsDto.getAccountType());
+        accounts.setBranchAdress(accountsDto.getBranchAdress());
+        return accounts;
+    }
+
+}
