6fb3229aaa99ed642a8a0a6e2602fffe2ef9524b
LuciosGIT <jloliveirajunior1903@gmail.com>
2024-09-08 10:06:35 -0300

    Implementing 'create' endpoint

--- accounts/src/main/java/com/joselucio/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/joselucio/accounts/controller/AccountsController.java
@@ -4,19 +4,24 @@ package com.joselucio.accounts.controller;
 import com.joselucio.accounts.constants.AccountsConstants;
 import com.joselucio.accounts.dto.CustomerDto;
 import com.joselucio.accounts.dto.ResponseDto;
+import com.joselucio.accounts.service.IAccountsService;
+import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@AllArgsConstructor
 @RestController
 @RequestMapping(path = "/api", produces = {MediaType.APPLICATION_JSON_VALUE})
 public class AccountsController {
 
+    private IAccountsService iAccountsService;
 
     @PostMapping("/create")
     public ResponseEntity<ResponseDto> createAccount(@RequestBody CustomerDto customerDto) {
 
+        iAccountsService.createAccount(customerDto);
         return ResponseEntity.status(HttpStatus.CREATED).body(new ResponseDto(
                 AccountsConstants.STATUS_201, AccountsConstants.MESSAGE_201));
 
