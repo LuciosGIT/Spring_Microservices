2aeeb391194fae0461b50fe32d0636db0d765a66
LuciosGIT <jloliveirajunior1903@gmail.com>
2024-09-08 10:06:15 -0300

    Adding create business logic

--- accounts/src/main/java/com/joselucio/accounts/service/impl/AccountsServiceImpl.java
+++ accounts/src/main/java/com/joselucio/accounts/service/impl/AccountsServiceImpl.java
@@ -1,11 +1,20 @@
 package com.joselucio.accounts.service.impl;
 
+import com.joselucio.accounts.constants.AccountsConstants;
 import com.joselucio.accounts.dto.CustomerDto;
+import com.joselucio.accounts.entity.Accounts;
+import com.joselucio.accounts.entity.Customer;
+import com.joselucio.accounts.exception.CustomerAlreadyExistsException;
+import com.joselucio.accounts.mapper.CustomerMapper;
 import com.joselucio.accounts.repository.AccountsRepository;
 import com.joselucio.accounts.repository.CustomerRepository;
 import com.joselucio.accounts.service.IAccountsService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+import java.util.Random;
 
 @Service
 @AllArgsConstructor
@@ -14,9 +23,39 @@ public class AccountsServiceImpl implements IAccountsService {
     private AccountsRepository accountsRepository;
     private CustomerRepository customerRepository;
 
-
+    /**
+     * @param customerDto - CustomerDto Object
+     */
     @Override
     public void createAccount(CustomerDto customerDto) {
-
+        Customer customer = CustomerMapper.mapToCustomer(customerDto, new Customer());
+        Optional<Customer> optionalCustomer = customerRepository.findByMobileNumber(customerDto.getMobileNumber());
+        if(optionalCustomer.isPresent()) {
+            throw new CustomerAlreadyExistsException("Customer already registered with given mobileNumber "
+                    +customerDto.getMobileNumber());
+        }
+        customer.setCreatedAt(LocalDateTime.now());
+        customer.setCreatedBy("Anonymous");
+        Customer savedCustomer = customerRepository.save(customer);
+        Accounts newAccount = createNewAccount(savedCustomer);
+        accountsRepository.save(newAccount);
     }
+
+    /**
+     * @param customer - Customer Object
+     * @return the new account details
+     */
+    private Accounts createNewAccount(Customer customer) {
+        Accounts newAccount = new Accounts();
+        newAccount.setCustomerId(customer.getCustomerId());
+        long randomAccNumber = 1000000000L + new Random().nextInt(900000000);
+
+        newAccount.setAccountNumber(randomAccNumber);
+        newAccount.setAccountType(AccountsConstants.SAVINGS);
+        newAccount.setBranchAddress(AccountsConstants.ADDRESS);
+        newAccount.setCreatedAt(LocalDateTime.now());
+        newAccount.setCreatedBy("Anonymous");
+        return newAccount;
+    }
+
 }
