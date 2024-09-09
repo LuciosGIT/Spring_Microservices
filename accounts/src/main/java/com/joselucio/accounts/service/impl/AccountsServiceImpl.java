2c47e66dc04a5256ce2a7af4b12500b585b671f9
LuciosGIT <jloliveirajunior1903@gmail.com>
2024-09-09 16:16:48 -0300

    Implementing auditing

--- accounts/src/main/java/com/joselucio/accounts/service/impl/AccountsServiceImpl.java
+++ accounts/src/main/java/com/joselucio/accounts/service/impl/AccountsServiceImpl.java
@@ -37,8 +37,6 @@ public class AccountsServiceImpl implements IAccountsService {
             throw new CustomerAlreadyExistsException("Customer already registered with given mobileNumber "
                     +customerDto.getMobileNumber());
         }
-        customer.setCreatedAt(LocalDateTime.now());
-        customer.setCreatedBy("Anonymous");
         Customer savedCustomer = customerRepository.save(customer);
         Accounts newAccount = createNewAccount(savedCustomer);
         accountsRepository.save(newAccount);
@@ -104,8 +102,6 @@ public class AccountsServiceImpl implements IAccountsService {
         newAccount.setAccountNumber(randomAccNumber);
         newAccount.setAccountType(AccountsConstants.SAVINGS);
         newAccount.setBranchAddress(AccountsConstants.ADDRESS);
-        newAccount.setCreatedAt(LocalDateTime.now());
-        newAccount.setCreatedBy("Anonymous");
         return newAccount;
     }
 
