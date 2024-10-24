8bd5dac74c646dfe330d80926b9f2887544e967d
José Lúcio <jloliveirajunior1903@gmail.com>
2024-10-24 18:18:21 -0300

    Implementing CustomerService interface implementation

---
+++ accounts/src/main/java/com/joselucio/accounts/service/impl/CustomerServiceImpl.java
@@ -0,0 +1,57 @@
+package com.joselucio.accounts.service.impl;
+
+import com.joselucio.accounts.dto.AccountsDto;
+import com.joselucio.accounts.dto.CardsDto;
+import com.joselucio.accounts.dto.CustomerDetailsDto;
+import com.joselucio.accounts.dto.LoansDto;
+import com.joselucio.accounts.entity.Accounts;
+import com.joselucio.accounts.entity.Customer;
+import com.joselucio.accounts.exception.ResourceNotFoundException;
+import com.joselucio.accounts.mapper.AccountsMapper;
+import com.joselucio.accounts.mapper.CustomerMapper;
+import com.joselucio.accounts.repository.AccountsRepository;
+import com.joselucio.accounts.repository.CustomerRepository;
+import com.joselucio.accounts.service.ICustomerService;
+import com.joselucio.accounts.service.client.CardsFeignClient;
+import com.joselucio.accounts.service.client.LoansFeignClient;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class CustomerServiceImpl implements ICustomerService {
+
+    private AccountsRepository accountsRepository;
+
+    private CustomerRepository customerRepository;
+
+    private CardsFeignClient cardsFeignClient;
+
+    private LoansFeignClient loansFeignClient;
+
+
+    @Override
+    public CustomerDetailsDto fetchCustomerDetails(String mobileNumber) {
+        Customer customer = customerRepository.findByMobileNumber(mobileNumber).orElseThrow(
+                () -> new ResourceNotFoundException("Customer", "mobileNumber", mobileNumber));
+
+        Accounts accounts = accountsRepository.findByCustomerId(customer.getCustomerId()).orElseThrow(
+                () -> new ResourceNotFoundException("Account", "customerId"
+                        , customer.getCustomerId().toString()));
+
+        CustomerDetailsDto customerDetailsDto = CustomerMapper.mapToCustomerDetailsDto(customer, new CustomerDetailsDto());
+        customerDetailsDto.setAccountsDto(AccountsMapper.mapToAccountsDto(accounts, new AccountsDto()));
+
+        ResponseEntity<LoansDto> loansDtoResponseEntity = loansFeignClient.fetchLoanDetails(mobileNumber);
+        customerDetailsDto.setLoansDto(loansDtoResponseEntity.getBody());
+
+        ResponseEntity<CardsDto> cardsDtoResponseEntity = cardsFeignClient.fetchCardDetails(mobileNumber);
+        customerDetailsDto.setCardsDto(cardsDtoResponseEntity.getBody());
+
+        return customerDetailsDto;
+
+
+    }
+
+}
