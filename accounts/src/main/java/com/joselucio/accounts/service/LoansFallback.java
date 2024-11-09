1bb2a0583ecbf175d97e3fd86c0e9f385b4ba112
José Lúcio <jloliveirajunior1903@gmail.com>
2024-11-09 09:37:28 -0300

    Implementing LoansFallback and Cards Fallback to support Feign Clients Circuit Breaking

---
+++ accounts/src/main/java/com/joselucio/accounts/service/LoansFallback.java
@@ -0,0 +1,14 @@
+package com.joselucio.accounts.service.client;
+
+import com.joselucio.accounts.dto.LoansDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+
+@Component
+public class LoansFallback implements LoansFeignClient{
+
+    @Override
+    public ResponseEntity<LoansDto> fetchLoanDetails(String correlationId, String mobileNumber) {
+        return null;
+    }
+}
